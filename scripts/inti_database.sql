4244a66cc993d35e1cb1979c1794ec3c91a53b55
Ziad-Goda-74 <zizogoda91@gmail.com>
2026-08-23 01:17:25 +0300

    Update inti_database.sql

--- scripts/inti_database.sql
+++ scripts/inti_database.sql
@@ -1,1 +1,13 @@
+use master;
+-- CREATE THE 'data_warehouse' DATABASE --
+create database data_warehouse;
 
+use data_warehouse;
+go
+--CREATE 3 SCHEMAS FOR 3 LAYERS --
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
